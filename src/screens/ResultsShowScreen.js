27223119b1883955f85a59a316ecf5b74fd8f313
Konrad Zajkowski <zajkuu.11@gmail.com>
2020-10-05 08:56:23 +0200

    Added single restaurant result to ResultsList.js

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, StyleSheet, FlatList, Image} from 'react-native';
+import yelp from '../api/yelp';
 
 const ResultsShowScreen = ({navigation}) => {
-  const id = navigation.getParam(id);
+  const id = navigation.getParam('id');
+  // when we expect single object in our state we set initialState to null
+  const [result, setResult] = useState(null);
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
 
   return (
     <View>
-      <Text>Result Show</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        renderItem={({item}) => {
+          return <Image style={styles.image} source={{uri: item}} />;
+        }}
+        keyExtractor={(photo) => photo}
+      />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: 200,
+    width: 300,
+  },
+});
 
 export default ResultsShowScreen;
